f4d810404747333d2333a410a884e2851e485432
Mohamed Adnan <moadnan919@gmail.com>
2024-02-26 21:40:27 +0200

    add to problems

---
+++ Playing With Characters/prog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char ch,s[100],sec[200];
+    scanf("%c %s %[^\n]%*c", &ch, &s, &sec);;
+    printf("%c \n%s \n%s \n", ch, s, sec); 
+
+    return 0;
+}
+
+/***
+     * char ch,s[100],sec[200];
+        scanf("%c", &ch);
+        fflush(stdin);fflush(stdout);
+        scanf("%s", &s);
+        fflush(stdin);fflush(stdout);
+        scanf("%[^\n]%*c", &sec);
+        printf("%c \n%s \n%s \n", ch, s, sec); 
+    */
